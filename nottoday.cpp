e05fde30a6276d8ea81a5652dfb9c3975edbe29a
Cheka <39255806+Argentix03@users.noreply.github.com>
2022-07-19 18:13:39 +0300

    Add files via upload

--- nottoday.cpp
+++ nottoday.cpp
@@ -1,7 +1,7 @@
 #include <windows.h>
-#include <TlHelp32.h>
-#include <iostream>
 #include <stdio.h>
+#define PROCESS_CREATION_MITIGATION_POLICY_BLOCK_NON_MICROSOFT_BINARIES_ALWAYS_ON
+#define PROCESS_CREATION_MITIGATION_POLICY_PROHIBIT_DYNAMIC_CODE_ALWAYS_ON
 
 int main(int argc, char* argv[])
 {
@@ -16,17 +16,20 @@ int main(int argc, char* argv[])
 	}
 	int pid = atoi(argv[2]);
 	printf("pid: %d", pid);
-	HANDLE parentProcessHandle = OpenProcess(MAXIMUM_ALLOWED, false, pid);
-	if (parentProcessHandle == INVALID_HANDLE_VALUE) {
+	HANDLE hParentProcess = OpenProcess(MAXIMUM_ALLOWED, false, pid);
+	if (hParentProcess == INVALID_HANDLE_VALUE) {
 		printf("Failed to get handle to process with pid: %d\n", pid);
 	}
 	InitializeProcThreadAttributeList(NULL, 1, 0, &attributeSize);
 	si.lpAttributeList = (LPPROC_THREAD_ATTRIBUTE_LIST)HeapAlloc(GetProcessHeap(), 0, attributeSize);
 	InitializeProcThreadAttributeList(si.lpAttributeList, 1, 0, &attributeSize);
-	UpdateProcThreadAttribute(si.lpAttributeList, 0, PROC_THREAD_ATTRIBUTE_PARENT_PROCESS, &parentProcessHandle, sizeof(HANDLE), NULL, NULL);
+	UpdateProcThreadAttribute(si.lpAttributeList, 0, PROC_THREAD_ATTRIBUTE_PARENT_PROCESS, &hParentProcess, sizeof(HANDLE), NULL, NULL);
 	si.StartupInfo.cb = sizeof(STARTUPINFOEXA);
 
 	CreateProcessA(NULL, (LPSTR)argv[1], NULL, NULL, FALSE, EXTENDED_STARTUPINFO_PRESENT, NULL, NULL, &si.StartupInfo, &pi);
+	
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
 
 	return 0;
 }
